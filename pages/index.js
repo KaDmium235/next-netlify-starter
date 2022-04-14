2ddc5a73908a3778866022aef57e1d8b89bf108b
KaDmium235 <103636195+KaDmium235@users.noreply.github.com>
2022-04-14 19:11:58 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
 
       <main>
         <Header title="Welcomuski to KaDmium235!!" />
+        <Header title="Welcomuski to KaDmium235!!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>          
