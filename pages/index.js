1bf42132d1d8837fd1e47e78b36a355bcde4abe4
KaDmium235 <103636195+KaDmium235@users.noreply.github.com>
2022-04-16 13:24:27 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,5 +8,23 @@ export default function Home() {
       /*<img src="bg_main.JPG" />*/
       <img src="bg_main.JPG" class="background" />
       </div>
+    
+    <div className="container">
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />          
+      </Head>
+      <main>         
+        <Header title="One line" />     
+        <Header title="Second line!!" />
+        <Header title="Third one" />
+        <Header title="Welcomushki to KaDmium235!!" />    
+        <p className="description">
+          Get started by editing <code>pages/index.js</code>
+        </p>     
+      </main>
+      <Footer />
+    </div>
+    
   )
 }
