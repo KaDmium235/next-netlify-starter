e751e1959f403de00c8311852730323126c20e58
KaDmium235 <103636195+KaDmium235@users.noreply.github.com>
2022-04-16 15:28:13 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,16 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (   
-    <html>
-  <head></head>
-  <body>
-    <header>
-      <hgroup>
-        <h1>Изображение на весь экран</h1>
-        <h2>Вариант 1</h2>
-      </hgroup>
-    </header>
-    <footer><a href="https://siterost.net" target="_blank">Siterost.net - сервисы для вебмастеров</a></footer>
+   <html>
+      <head></head>
+          <body>
+            <header>
+              <hgroup>
+                  <h1>КаДмий</h1>
+                  <h2>Вариант 1</h2>
+              </hgroup>
+            </header>
+    /*<footer><a href="https://siterost.net" target="_blank">Siterost.net - сервисы для вебмастеров</a></footer>*/
   </body>
 </html>
     
