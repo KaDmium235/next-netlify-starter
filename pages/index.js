111d71f8a86d1d371f056d7c7ca5c4621fde3051
KaDmium235 <103636195+KaDmium235@users.noreply.github.com>
2022-04-16 12:01:10 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,22 +4,8 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (   
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />          
-      </Head>
-      <main>         
-        <Header title="One line" />     
-        <Header title="Second line!!" />
-        <Header title="Third one" />
-        <Header title="Welcomushki to KaDmium235!!" />    
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>     
-      </main>
-      <Footer />
-    </div>
-    <img src="bg_main.JPG" alt="Netlify Logo"  /> /*  className={styles.logo}      */
+    <div class="background">
+      <img src="bg_main.JPG" />
+      </div>
   )
 }
