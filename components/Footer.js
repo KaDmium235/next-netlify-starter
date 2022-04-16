615bc5774bfabb60c95fb21c6e841ed091d2c732
KaDmium235 <103636195+KaDmium235@users.noreply.github.com>
2022-04-16 11:02:24 +0300

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/bg_main.JPG" alt="Netlify Logo" className={styles.bground1} /> for you /*logo */
+        Made with <img src="/bg_main.JPG" alt="Netlify Logo" className={styles.logo} /> for you /*logo */
       </footer>
     </>
   )
